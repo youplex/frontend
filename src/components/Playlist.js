466f2939b6845872bd09c303c87db71712cde38f
Prajna1999 <gituprajna20@mail.com>
2022-08-03 00:07:36 +0530

    add playlist page

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,4 +1,5 @@
-
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -7,27 +8,50 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import img1 from '../assets/img1.jpg';
+import img2 from '../assets/img2.jpg';
+import img3 from '../assets/img3.jpg';
+import img4 from '../assets/img4.jpg';
+import { PlaylistData } from './PlaylistData';
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
+
+
+// const bull = (
+//   <Box
+//     component="span"
+//     sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+//   >
     
-  </Box>
-);
+//   </Box>
+// );
 
  function Playlist() {
   return (
-    <div className='ml-52 w-max '>
-    <Card >
-      <CardContent>
-        
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <div className='ml-52 w-4/5 h-min grid grid-cols-2 gap-4 bg-sidebar'>
+        {PlaylistData.map((item,index)=>{
+            return (
+
+                <Card sx={{maxWidth:350}} >
+                
+                <CardMedia
+                    
+                    component="img"
+                    
+                    image={item.img}
+                    alt="Paella dish"
+                />
+                {/* <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        Hello
+                    </Typography>
+                </CardContent> */}
+                <span>{item.title}</span>
+                
+                </Card>
+               
+            )
+        })}
+          
     </div>
   );
 }
---
+++ src/components/PlaylistData.js
@@ -0,0 +1,29 @@
+import img1 from '../assets/img1.jpg';
+import img2 from '../assets/img1.jpg';
+import img3 from '../assets/img1.jpg';
+import img4 from '../assets/img1.jpg';
+
+
+export const PlaylistData=[
+    {
+        id:1,
+        img:img1,
+        title:'StarWars full cast group photo oil painting'
+    },
+
+    {
+        id:2,
+        img:img2,
+        title:'StarWars full cast group photo oil painting'
+    },
+    {
+        id:3,
+        img:img3,
+        title:'StarWars full cast group photo oil painting'
+    },
+    {
+        id:4,
+        img:img4,
+        title:'StarWars full cast group photo oil painting'
+    }
+]
